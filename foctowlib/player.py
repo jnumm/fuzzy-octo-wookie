75bab9876e3f6c4403fd6a3559f178697e7c70ed
Juhani Numminen <juhaninumminen0@gmail.com>
2013-10-19 11:03:21 +0300

    Create player.py

---
+++ foctowlib/player.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import pygame
+
+import misc
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = misc.load_image('player.png')
+
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.rect.move_ip(-2, 0)
+        if keys[pygame.K_RIGHT]:
+            self.rect.move_ip(2, 0)
