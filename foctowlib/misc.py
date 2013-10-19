94fef3428a65f2c18e5398713f1cc3a7cdc9e717
Juhani Numminen <juhaninumminen0@gmail.com>
2013-10-19 11:59:24 +0300

    misc.py: Add documentation

--- foctowlib/misc.py
+++ foctowlib/misc.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
+"""Some utility functions for the game.
+"""
 
 import os
 
 import pygame
 
 def load_image(name):
+    """Loads an image file.
+    Returns: a tuple of Image and the corresponding Rect.
+    """
     fullname = os.path.join("assets", "img", name)
     image = pygame.image.load(fullname)
     #image = image.convert()
