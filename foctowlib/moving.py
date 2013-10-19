e25a4946bb08b72e9aafaf7d2944b3556b4e42f6
Juhani Numminen <juhaninumminen0@gmail.com>
2013-10-19 12:41:57 +0300

    moving.Moving: generalize code

--- foctowlib/moving.py
+++ foctowlib/moving.py
@@ -1,19 +1,28 @@
 # -*- coding: utf-8 -*-
-"""A moving backgound.
-"""
 
 import pygame
 
 import const
 import misc
 
-class Back(pygame.sprite.Sprite):
-    def __init__(self, ypos=0):
+class Moving(pygame.sprite.Sprite):
+    """A moving object, either a background image or a train car.
+    """
+
+    def __init__(self, type, ypos=0):
         pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = misc.load_image("bg.png")
+        self.type = type
+        if self.type == const.MOVING_BG:
+            self.image, self.rect = misc.load_image("bg.png")
+        elif self.type == const.MOVING_TRAINCAR:
+            self.image, self.rect = misc.load_image("player.png")
         self.rect.top = ypos
- 
+
     def update(self):
-        self.rect.move_ip(0, 5)
+        if self.type == const.MOVING_BG:
+            self.rect.move_ip(0, 5)
+        elif self.type == const.MOVING_TRAINCAR:
+            self.rect.move_ip(0, 2)
+
         if self.rect.top > const.DISPLAY_HEIGTH:
             self.rect.bottom = 0
