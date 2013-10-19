530aad079d3e6afa4e0db10b4506988c4d56410f
Juhani Numminen <juhaninumminen0@gmail.com>
2013-10-19 11:25:09 +0300

    Update main.py

--- foctowlib/main.py
+++ foctowlib/main.py
@@ -2,27 +2,26 @@
 
 import pygame
 
+import const
 from player import *
 import traincar
-
-DISPLAY_SIZE = DISPLAY_WIDTH, DISPLAY_HEIGTH = 640, 800
 
 ukko = pygame.image.load("C:\Users\Pelitalo\Documents\GameJam\\assets\img\player.png")
 bg = pygame.image.load("assets\img\\bg.png")
 
 def main():
     pygame.init()
-    display = pygame.display.set_mode(DISPLAY_SIZE)
+    display = pygame.display.set_mode(const.DISPLAY_SIZE)
 
     pygame.display.set_caption("GameJam")
     
     sijainti = [50, 50]
 
     player = Player()
-    player.rect.centery = DISPLAY_HEIGTH / 2
+    player.rect.centery = const.DISPLAY_HEIGTH / 2
 
-    car1 = traincar.TrainCar(0, DISPLAY_HEIGTH)
-    car2 = traincar.TrainCar(200, DISPLAY_HEIGTH)
+    car1 = traincar.TrainCar()
+    car2 = traincar.TrainCar(200)
 
     allsprites = pygame.sprite.RenderPlain((player, car1, car2))
 
@@ -34,9 +33,10 @@ def main():
         for tapahtuma in pygame.event.get():
             if tapahtuma.type == pygame.QUIT:
                 return
-        
+
         allsprites.update()
 
+        display.fill((0, 0, 0))
         display.blit(bg, (0, 0))
         display.blit(ukko, sijainti)
         allsprites.draw(display)
