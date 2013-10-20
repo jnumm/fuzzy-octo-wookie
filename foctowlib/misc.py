38989bd536f6e9bccb1e746a95db7e2b989dad3e
Juhani Numminen <juhaninumminen0@gmail.com>
2013-10-20 16:31:58 +0300

    Add misc.load_sound

--- foctowlib/misc.py
+++ foctowlib/misc.py
@@ -15,6 +15,19 @@ def load_image(name):
     #image = image.convert()
     return image, image.get_rect()
 
+def load_sound(name):
+    class NoneSound:
+        def play(self): pass
+    if not pygame.mixer:
+        return NoneSound()
+    fullname = os.path.join("assets", "snd", name)
+    try:
+        sound = pygame.mixer.Sound(fullname)
+    except pygame.error, message:
+        print "Cannot load sound: ", fullname
+        raise SystemExit, message
+    return sound
+
 # From http://shinylittlething.com/2009/07/21/pygame-and-animated-sprites/
 def load_sliced_sprites(w, h, filename):
     '''
