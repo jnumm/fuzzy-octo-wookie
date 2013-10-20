9bfcc7ec7acdbb65dfa191afd25ef2c0ec33f328
Tuomas Numminen <tuomasx.n@gmail.com>
2013-10-20 10:37:23 +0300

    Cooler jump

--- foctowlib/player.py
+++ foctowlib/player.py
@@ -10,23 +10,31 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image, self.rect = misc.load_image('player.png')
+        self.orig_image = self.image
         self.jump = 0
 
     def update(self):
         keys = pygame.key.get_pressed()
         
-        if self.jump > 0 and self.jump < 15:
+        if self.jump > 0 and self.jump < 30:
             self.rect.move_ip(0, -1)
+            self.image = pygame.transform.scale(self.orig_image,
+                    (self.image.get_width() + 1,
+                    self.image.get_height() + 1))
             self.jump += 1
-        elif self.jump >= 15 and self.jump < 30:
+        elif self.jump >= 30 and self.jump < 60:
             self.rect.move_ip(0, 1)
+            self.image = pygame.transform.scale(self.orig_image,
+                    (self.image.get_width() - 1,
+                    self.image.get_height() - 1))
             self.jump += 1
-        elif self.jump == 30:
+        elif self.jump == 60:
+            self.image = self.orig_image
             self.jump = 0
         
-        if keys[pygame.K_LEFT]:
+        if keys[pygame.K_LEFT] and self.jump == 0:
             self.rect.move_ip(-2, 0)
-        if keys[pygame.K_RIGHT]:
+        if keys[pygame.K_RIGHT] and self.jump == 0:
             self.rect.move_ip(2, 0)
         if keys[pygame.K_SPACE] and self.jump == 0:
             self.jump = 1
