c6587c0b8f707d38b844d50de0533ece19475f9e
Juhani Numminen <juhaninumminen0@gmail.com>
2013-10-20 12:57:03 +0300

    Update chopper.py

--- foctowlib/chopper.py
+++ foctowlib/chopper.py
@@ -13,6 +13,31 @@ class Chopper(pygame.sprite.Sprite):
         self.image, self.rect = misc.load_image("chopper.png")
         self.rect.left = const.DISPLAY_WIDTH
         self.rect.centery = const.DISPLAY_HEIGTH / 2
+        self.landing = 0
+        self.orig_image = self.image
 
     def update(self):
-        self.rect.move_ip(-4, 0)
+        if self.landing > 0 and self.landing < 30:
+            self.image = pygame.transform.scale(self.orig_image,
+                    (self.image.get_width() - 1,
+                    self.image.get_height() - 1))
+            self.landing += 1
+        elif self.landing >= 30 and self.landing < 60:
+            self.image = pygame.transform.scale(self.orig_image,
+                    (self.image.get_width() + 1,
+                    self.image.get_height() + 1))
+            self.landing += 1
+        elif self.landing == 60:
+            self.image = self.orig_image
+            self.landing = 0
+        else:
+            self.rect.move_ip(-4, 0)
+
+    def land(self):
+        self.landing = 1
+
+    def is_landing(self):
+        return (self.landing > 0)
+
+    def ready_to_pick(self):
+        return (self.landing == 30)
