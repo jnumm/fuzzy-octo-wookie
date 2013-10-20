157a5a94393f4b43cba462cdcd348c5babaa58ef
Juhani Numminen <juhaninumminen0@gmail.com>
2013-10-20 13:48:20 +0300

    Update misc.py

--- foctowlib/misc.py
+++ foctowlib/misc.py
@@ -14,3 +14,20 @@ def load_image(name):
     image = pygame.image.load(fullname)
     #image = image.convert()
     return image, image.get_rect()
+
+# From http://shinylittlething.com/2009/07/21/pygame-and-animated-sprites/
+def load_sliced_sprites(w, h, filename):
+    '''
+    Specs :
+    	Master can be any height.
+    	Sprites frames width must be the same width
+    	Master width must be len(frames)*frame.width
+    Assuming you resources directory is named "resources"
+    '''
+    images = []
+    master_image = pygame.image.load(os.path.join("assets", "img", filename)).convert_alpha()
+    
+    master_width, master_height = master_image.get_size()
+    for i in xrange(int(master_width/w)):
+    	images.append(master_image.subsurface((i*w,0,w,h)))
+    return images
